9f1d5e1fc50258eef0adb7dcebaf1f9e6e170d89
akakou <akakou571@gmail.com>
2022-10-16 23:24:12 +0900

    [add] read access token

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
 const AuthURL = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s&scope=profile openid&nonce=%s"
@@ -26,6 +31,44 @@ func main() {
 	state := "12345"
 	nonce := "09876"
 
-	url := fmt.Sprintf(AuthURL, config.ClientID, config.RedirectURL, state, nonce)
-	fmt.Println(url)
+	authUrl := fmt.Sprintf(AuthURL, config.ClientID, config.RedirectURL, state, nonce)
+	fmt.Println(authUrl)
+
+	reader := bufio.NewReader(os.Stdin)
+	code, _ := reader.ReadString('\n')
+
+	code = strings.Replace(code, "\n", "", -1)
+
+	values := url.Values{}
+	values.Add("grant_type", "authorization_code")
+	values.Add("code", code)
+	values.Add("client_id", config.ClientID)
+	values.Add("client_secret", config.ClientSecret)
+	values.Add("redirect_uri", config.RedirectURL)
+
+	req, err := http.NewRequest(
+		"POST",
+		TokenURL,
+		strings.NewReader(values.Encode()),
+	)
+	
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
+
 }
