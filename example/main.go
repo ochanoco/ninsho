57470a1919593202cf32da78015985940eb889b9
akakou <akakou571@gmail.com>
2022-10-17 03:16:45 +0900

    [add] session controll

--- example/main.go
+++ example/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"gin_line_login"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	gin_line_login.Hi()
+	lineLogin := gin_line_login.DefaultLineLogin()
+
+	r := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/", lineLogin.AuthMiddleware(), func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "loggined!"})
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		lineLogin.Login(c)
+		c.JSON(200, gin.H{"message": "login"})
+	})
+
+	r.GET("/logout", func(c *gin.Context) {
+		lineLogin.Logout(c)
+		c.JSON(200, gin.H{"message": "logout"})
+	})
+
+	r.GET("/unauthorized", func(c *gin.Context) {
+		c.JSON(401, gin.H{"message": "unauthorized"})
+	})
+	r.Run()
 }
--- login.go
+++ login.go
@@ -1,7 +1,47 @@
 package gin_line_login
 
-import "fmt"
+import (
+	"net/http"
 
-func Hi() {
-	fmt.Printf("hi!")
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+type LineLogin struct {
+	UnauthorizedPath string
+}
+
+func NewLineLogin(unauthorized string) *LineLogin {
+	return &LineLogin{
+		unauthorized,
+	}
+}
+
+func DefaultLineLogin() *LineLogin {
+	return NewLineLogin("/unauthorized")
+}
+
+func (lineLogin *LineLogin) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		if session.Get("id_token") == nil {
+			c.Redirect(http.StatusFound, lineLogin.UnauthorizedPath)
+			c.Abort()
+		}
+
+		c.Next()
+	}
+}
+
+func (lineLogin *LineLogin) Login(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Set("id_token", "123")
+	session.Save()
+}
+
+func (lineLogin *LineLogin) Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Delete("id_token")
+	session.Save()
 }
