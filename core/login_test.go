071fa6534856d40781953321897477f767d7aac4
akakou <akakou571@gmail.com>
2022-10-18 23:14:24 +0900

    [add] generate session for test

--- core/login_test.go
+++ core/login_test.go
@@ -4,9 +4,8 @@ import (
 	"testing"
 )
 
-func TestAuthURL(t *testing.T) {
+func sampleSession() Session {
 	var provider Provider
-	var expected = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=12345&redirect_uri=http://127.0.0.1:8080/callback&state=aaa&scope=profile openid&nonce=bbb"
 
 	provider.ClientID = "12345"
 	provider.ClientSecret = ""
@@ -20,9 +19,20 @@ func TestAuthURL(t *testing.T) {
 	session.State = "aaa"
 	session.Nonce = "bbb"
 
+	return session
+}
+
+func TestAuthURL(t *testing.T) {
+	var expected = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=12345&redirect_uri=http://127.0.0.1:8080/callback&state=aaa&scope=profile openid&nonce=bbb"
+
+	session := sampleSession()
 	url := session.AuthURL()
 
 	if url != expected {
 		t.Fatalf("Auth URL is not collect\nexpected: %v\nactual:   %v", url, expected)
 	}
+}
+
+func TestGetUser(t *testing.T) {
+	// todo
 }
