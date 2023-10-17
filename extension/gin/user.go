03dd966249da511718c5113a6b9d6fcb712b19ac
akakou <akakou571@gmail.com>
2023-10-17 22:48:32 +0900

    feat: return nil, when authentication failed

--- extension/gin/user.go
+++ extension/gin/user.go
@@ -12,16 +12,17 @@ func GetUser[T any](c *gin.Context) (*T, error) {
 	var jwt T
 
 	sess := sessions.Default(c)
+
 	user := sess.Get("user")
-	if user == "" {
-		return nil, fmt.Errorf("user not logged in")
+	if user == nil {
+		return nil, nil
 	}
 
-	err := json.Unmarshal([]byte(user.(string)), &jwt)
+	buf := []byte(user.(string))
+	err := json.Unmarshal(buf, &jwt)
 
 	if err != nil {
-		fmt.Println(err)
-		return nil, fmt.Errorf("failed to deserialize user")
+		return nil, fmt.Errorf("failed to deserialize user: %v", err)
 	}
 
 	return &jwt, nil
